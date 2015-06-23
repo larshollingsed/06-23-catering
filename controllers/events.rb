903bf6749d8ebe9770f5df4ede0bbaca323f07ec
Lars Hollingsed <larshollingsed@gmail.com>
2015-06-23 18:54:27 -0500

    Added 'see all events'

--- controllers/events.rb
+++ controllers/events.rb
@@ -6,3 +6,7 @@ get"/add_event_confirm" do
   @event_added = Event.add("date" => params["date"], "name" => params["name"], "hours" => params["hours"].to_f, "hourly_wage" => params["hourly_wage"].to_f, "gratuity" => params["gratuity"].to_i, "alcohol" => params["alcohol"])
   erb :"/main/home"
 end
+
+get "/see_all_events" do
+  erb :"/events/see_all_events"
+end
