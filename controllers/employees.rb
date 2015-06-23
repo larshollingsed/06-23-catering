1bde308911507aa967782abe9b48fdb70bdf9735
Lars Hollingsed <larshollingsed@gmail.com>
2015-06-23 14:58:21 -0500

    'Modify employee' functionality added

--- controllers/employees.rb
+++ controllers/employees.rb
@@ -2,8 +2,8 @@ get "/see_all_employees" do
   erb :"/employees/see_all_employees"
 end
 
-get "/add_employee" do
-  erb :"/employees/add_employee"
+get "/add_employee_form" do
+  erb :"/employees/add_employee_form"
 end
 
 get "/add_employee_confirm" do
@@ -11,11 +11,25 @@ get "/add_employee_confirm" do
   erb :"/main/home"
 end
 
-get "/delete_employee" do
-  erb :"/employees/delete_employee"
+get "/delete_employee_form" do
+  erb :"/employees/delete_employee_form"
 end
 
 get "/delete_employee_confirm" do
   Employee.find(params["id"]).delete
+  erb :"/main/home"
+end
+
+get "/modify_employee_form" do
+  erb :"/employees/modify_employee_form"
+end
+
+get "/modify_employee_form2" do
+  erb :"/employees/modify_employee_form2"
+end
+
+get "/modify_employee_confirm" do
+  updated_employee = Employee.new("id" => params["id"].to_i, "name" => params["name"], "age" => params["age"].to_i)
+  updated_employee.save
   erb :"/main/home"
 end
