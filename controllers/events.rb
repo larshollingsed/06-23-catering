3fc93ebf78438957dcdde5aca1b02831af69fa8b
Lars Hollingsed <larshollingsed@gmail.com>
2015-06-24 15:08:48 -0500

    Delete and re-add distributions when modifying an event

--- controllers/events.rb
+++ controllers/events.rb
@@ -39,5 +39,17 @@ end
 get "/modify_event_confirm" do
   @event_modified = Event.new("id" => params["id"].to_i, "name" => params["name"], "date" => params["date"], "hours" => params["hours"].to_f, "hourly_wage" => params["hourly_wage"].to_f, "gratuity" => params["gratuity"].to_f, "alcohol" => params["alcohol"])
   @event_modified.save
+  
+  
+  #TODO - delete all rows from distributions where event_id = @event_modified.id
+  DB.execute("DELETE FROM distributions where event_id = #{@event_modified.id};")
+  
+  params["employee_id"].each do |x|
+    if params["manager"] == x
+      Distribution.add({"event_id" => @event_modified.id, "employee_id" => x.to_i, "manager" => "yes"})
+    else
+      Distribution.add({"event_id" => @event_modified.id, "employee_id" => x.to_i})
+    end
+  end
   erb :"/main/home"
 end
